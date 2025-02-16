3ba021461261cbca13f1f09da239342d1f28a5e3
Marcus Spracklen <marcus.spracklen@gmail.com>
2025-02-15 18:22:24 -0800

    update

--- assignment_01.cpp
+++ assignment_01.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-  constexpr int ADD_ONE = 1;
-  constexpr int VAR_SIZE = 8192;
-  constexpr int ASCII_VAL_A = 65;
-  constexpr int ASCII_VAL_a = 97;
-  constexpr int ASCII_VAL_Z = 90;
-  constexpr int ASCII_VAL_z = 122;
+  constexpr int BUFFER_SIZE = 8192;
+  constexpr int ASCII_VAL_A = 'A';
+  constexpr int ASCII_VAL_a = 'a';
+  constexpr int ASCII_VAL_Z = 'Z';
+  constexpr int ASCII_VAL_z = 'z';
 
-  int counter[ASCII_VAL_z - ASCII_VAL_A] = {0};
-  char input[VAR_SIZE];
+  int counter[26] = {0};
+  char input[BUFFER_SIZE];
   double total_chars = 0;
-  double percent = 0;
 
   // Reads into the array "input", the for loop is here if the input exceeds the maximum value of
   // the data type.
-  for (std::cin.read(input, VAR_SIZE); std::cin.gcount(); std::cin.read(input, VAR_SIZE)) {
+  for (std::cin.read(input, BUFFER_SIZE); std::cin.gcount(); std::cin.read(input, BUFFER_SIZE)) {
     // Iterates an amount of times equal to the number of characters that were input.
     // This loop's job is to count up the total number of characters and what letter they were by
     // putting them into array and storing them there so we can access them later.
     for (int i = 0; i < std::cin.gcount(); ++i) {
       // Checks to makes sure that the input was part of the alphabet and does not have another
       // ASCII value outside of A-Z.
-      if (input[i] > ASCII_VAL_A && input[i] < ASCII_VAL_z) {
-        int place_holder = 0;
+      if ((input[i] >= ASCII_VAL_A && input[i] <= ASCII_VAL_Z) || (input[i] >= ASCII_VAL_a && input[i] <= ASCII_VAL_z)) {
         // Passes placeholder the value of the array at "i".
-        place_holder = input[i];
+        int index = (input[i] >= ASCII_VAL_a) ? (input[i] - ASCII_VAL_a) : (input[i] - ASCII_VAL_A);
         // Adds one to the value at "i".
-        counter[place_holder - ASCII_VAL_A] += ADD_ONE;
-        total_chars += ADD_ONE;
+        counter[index]++;
+        total_chars++;
       }
     }
   }
@@ -45,13 +43,13 @@ int main() {
     letter_count = counter[i] + counter[n];
 
     // Calculates the percent that each letter appears in the whole input.
-    percent = (letter_count / total_chars) * 100;
-
+    //int percent = static_cast<int>(std::ceil((static_cast<double>(letter_count) / total_chars) * 100));
+    int percent = (static_cast<double>(letter_count) / total_chars) * 100;
     // Checks to make sure that in the input the letter we are currently checking appeared.
     if (letter_count != 0) {
       std::cout << letter_label << " " << letter_count << " ";
       // Prints out the percentange in "*""
-      for (int i = 0; i < percent; ++i) {
+      for (int star_count = 0; star_count < percent; ++star_count) {
         std::cout << "*";
       }
       std::cout << "\n";
