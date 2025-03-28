fdc22f82fa718dfcbd5c4ae851c6627a78ceb66f
Marcus Spracklen <marcus.spracklen@gmail.com>
2025-03-28 16:49:22 -0700

    assignment06.cpp

--- cs19_integer.h
+++ cs19_integer.h
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <iostream>
 
 namespace cs19 {
  
@@ -10,39 +11,45 @@ namespace cs19 {
       std::vector<int> nums;
 
       Integer(const std::string &input){
-        for (int i = input.size(); i < 0; i--) {
+        for (int i = input.size() - 1; i != 0; i--) {
             nums.push_back(input[i]);
         }
       }
      
       operator double() const {
         double result = 0;
+        int column = 1;
 
-        for(int i = 0; i < nums.size(); i++) {
-            result += nums[i];
+        for(auto i = nums.rbegin(); i != nums.rend(); i--) {
+            result += (*i * column);
+            column = column * 10;
         }
-        return result;  // TODO: return the closest approximation to this integer, as a `double`
+        return {result};  // TODO: return the closest approximation to this integer, as a `double`
       }
-     
+
       operator std::string() const {
         std::string result;
 
-        for (int i = 0; i < nums.size(); i++) {
-            result = std::to_string(nums[i]);
+        for (int i = 0; i != nums.size(); i++) {
+            result.push_back(nums[i]);
         }
         return result;  // TODO: return a string containing the digits in this integer
       }
      
-      Integer operator+(const Integer &that) const {
-        std::string sum;
-        int term_1 = ;
-        int term_2 = that;
+    //   Integer operator+(const Integer &that) const {
+    //     std::string sum;
+    //    // Integer term_1;
+    //     int term_2 = that;
 
 
 
         
-        return {"0"};  // TODO: return the sum of this integer and that integer as a new integer
-      }
+    //     //return {"0"};  // TODO: return the sum of this integer and that integer as a new integer
+    //   }
+
+    //   Integer operator==(const Integer &that) {
+
+    //   }
      
     };
      
