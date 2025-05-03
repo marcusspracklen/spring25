313ad6fed60df55aff3baa92e3176870dae7c121
Marcus Spracklen <marcus.spracklen@gmail.com>
2025-05-02 17:44:35 -0700

    cs19_dna_sequence.cpp

--- cs19_dna_sequence.cpp
+++ cs19_dna_sequence.cpp
@@ -1,14 +1,18 @@
 #include "cs19_dna_sequence.h"
 
 namespace cs19 {
+    // Default constructor
     DnaSequence::DnaSequence() : head(nullptr), tail(nullptr), length(0) {}
 
+    // Constructor that builds list from a string
     DnaSequence::DnaSequence(const std::string& sequence) : DnaSequence() {
         for (char i : sequence) {
             push_back(i);
         }
     }
 
+
+    // Equality comparison, only returns true if the sequences are identical
     bool DnaSequence::operator==(const DnaSequence& another) {
         if(length != another.length) {
             return false;
@@ -25,4 +29,113 @@ namespace cs19 {
         }
         return true;
     }
+
+    // Accesses the character at the position pos
+    char& DnaSequence::operator[](std::size_t pos) {
+        Node* current = head;
+        for (std::size_t i = 0; i < pos && current; i++) {
+            current = current->next;
+        }
+        return current->data;
+    }
+
+    // Adds a character to the end of the list
+    void DnaSequence::push_back(char val) {
+        Node* node = new Node(val);
+
+        if (!tail) {
+            head = tail = node;
+        } else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+        ++length;
+    }
+
+    // Returns size of the list
+    std::size_t DnaSequence::size() {
+        return length;
+    }
+
+    void DnaSequence::splice_after(DnaSequence& that) {
+        if (that.length == 0) {
+            return;
+        }
+
+        if (length == 0) {
+            head = that.head;
+            tail = that.tail;
+        } else {
+            tail->next = that.head;
+            that.head->prev = tail;
+            tail = that.tail;
+        }
+
+        length += that.length;
+
+        that.head = that.tail = nullptr;
+        that.length = 0;
+    }
+
+    void DnaSequence::splice_before(DnaSequence& that) {
+        if (that.length == 0) {
+            return;
+        }
+
+        if (length == 0) {
+            head = that.head;
+            tail = that.tail;
+        } else {
+            that.tail->next = head;
+            head->prev = that.tail;
+            head = that.head;
+        }
+
+        length += that.length;
+
+        that.head = that.tail = nullptr;
+        that.length = 0;
+    }
+
+    void DnaSequence::remove_all(const DnaSequence& motif) {
+        if (motif.length == 0 || motif.length > this->length) {
+            return;
+        }
+
+        Node* current = head;
+
+        while (current) {
+            Node* start = current;
+            Node* check = current;
+            Node* motif_node = motif.head;
+
+            // Match motif to current node
+            while (check && motif_node && check->data == motif_node->data) {
+                check = check->next;
+                motif_node = motif_node->next;
+            }
+
+            // Match found
+            if(!motif_node) {
+                Node* before = start->prev;
+                Node* after = check;
+
+                // Delete match
+                Node* to_delete = start;
+                for (std::size_t i = 0; i < motif.length; i++) {
+                    Node* next = to_delete->next;
+                    delete to_delete;
+                    to_delete = next;
+                }
+
+                length -= motif.length;
+                current = after;
+            // No match found move forward
+            } else {
+                current = current->next;
+            }
+        }
+    }
+
 }
